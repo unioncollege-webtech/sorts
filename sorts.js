d78f947139a7d94a56f241d5ef37ff6f4b056f41
bretttrana <brett.trana@gmail.com>
2016-10-03 19:52:46 +0000

    bubbleSort and quickSort functions that work with # and char

--- sorts.js
+++ sorts.js
@@ -3,3 +3,81 @@ var group2 = [32, 29, 28, 47, 50, 43, 40, 45, 37, 46, 27, 49, 35, 26, 34, 44, 48
 var group3 = ['sadly', 'roast', 'Loopy', 'burger', 'gavel', 'lure', 'postpone', 'frontal', 'Gavel', 'lavender'];
 
 // Your code goes here! Hooray!
+
+function bubbleSort(array) {
+    var newArray = array.slice();
+    var inOrder = false;
+    while(inOrder == false) {
+        inOrder = true;
+        if (typeof newArray[0] === 'number') {
+            for(var i = 0; i < newArray.length; i++) {    
+                if (newArray[i] > newArray[i + 1]) {
+                    var temp = newArray[i];
+                    newArray[i] = newArray[i + 1];
+                    newArray[i + 1] = temp;
+                    inOrder = false;
+                }
+            }
+        }
+        else {
+            for(var i = 0; i < newArray.length; i++) {
+                if (newArray[i].localeCompare(newArray[i + 1]) > 0) {
+                    var temp = newArray[i];
+                    newArray[i] = newArray[i + 1];
+                    newArray[i + 1] = temp;
+                    inOrder = false;
+                }
+            }
+        }
+        
+    }
+    return newArray;
+}
+
+
+function quickSort(array) {
+    if (typeof array[0] == 'number') {
+        var pivot,
+            below = [], 
+            above = [], 
+            i, 
+            l;
+        if (array.length < 2) {
+            return array;
+        }
+        pivot = array.pop();
+        for (i = 0, l = array.length; i < l; i++) {
+            if (array[i] < pivot) {
+                below.push(array[i]);
+            } else {
+                above.push(array[i]);
+            }
+        }
+        return [].concat(quickSort(below), pivot, quickSort(above));
+    } 
+    else {
+        var pivot,
+            below = [], 
+            above = [], 
+            i, 
+            l;
+        if (array.length < 2) {
+            return array;
+        }
+        pivot = array.pop();
+        for (i = 0, l = array.length; i < l; i++) {
+            if (array[i].localeCompare(pivot) < 0) {
+                below.push(array[i]);
+            } else {
+                above.push(array[i]);
+            }
+        }
+        return [].concat(quickSort(below), pivot, quickSort(above));
+    }
+    
+}
+
+
+console.log(quickSort(group1));
+console.log(quickSort(group2));
+console.log(quickSort(group3));
